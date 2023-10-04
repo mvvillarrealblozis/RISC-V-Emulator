2e4b6eeb5170a413b189f1f752461b490616f018
mvvillarrealblozis <112027248+mvvillarrealblozis@users.noreply.github.com>
2023-10-04 15:30:05 -0700

    add type functions

--- rv_emu.c
+++ rv_emu.c
@@ -12,6 +12,131 @@ static void unsupported(char *s, uint32_t n) {
     exit(-1);
 }
 
+void emu_r_type(struct rv_state *rsp, uint32_t iw) {
+    uint32_t rd = get_bits(iw, 7, 5);
+    uint32_t rs1 = get_bits(iw, 15, 5);
+    uint32_t rs2 = get_bits(iw, 20, 5);
+    uint32_t funct3 = get_bits(iw, 12, 3);
+    uint32_t funct7 = get_bits(iw, 25, 7);
+
+    if (funct3 == 0b000) {
+    	if (funct7 == 0b0000000) { // if its add 
+    		rsp->regs[rd] = rsp->regs[rs1] + rsp->regs[rs2];
+    	} else if (funct7 == 0b0100000) { // if its sub
+    		rsp->regs[rd] = rsp->regs[rs1] - rsp->regs[rs2];
+    	} else if (funct7 == 0b0000001) { // if its mul
+    		rsp->regs[rd] = rsp->regs[rs1] * rsp->regs[rs2];
+    	} else {
+    		unsupported("R-type", funct7);
+    	}
+    } else if (funct3 == 0b111){
+    	//and 
+        rsp->regs[rd] = rsp->regs[rs1] & rsp->regs[rs2];
+    } else if (funct3 == 0b110) {
+    	//or 
+    	rsp->regs[rd] = rsp->regs[rs1] | rsp->regs[rs2];
+    } else if (funct3 == 0b101) {
+    	if (funct7 == 0b0000000) { //srl 
+    		rsp->regs[rd] = rsp->regs[rs1] >> rsp->regs[rs2];
+    	}
+    } else if (funct3 == 0b001) { //ssl
+    	rsp->regs[rd] = rsp->regs[rs1] << rsp->regs[rs2];
+    } else {
+    	unsupported("R-type", funct3);
+    }
+    rsp->pc += 4; // Next instruction
+}
+
+void emu_i_type(struct rv_state *rsp, uint32_t iw) {
+	uint32_t rd = get_bits(iw, 7, 5);
+	uint32_t rs1 = get_bits(iw, 15, 5);
+	int32_t imm12 = sign_extend(get_bits(iw, 20, 12), 11);
+	uint32_t funct3 = get_bits(iw, 12, 3);
+
+	if (funct3 == 0b101) {
+		// srli 
+		uint32_t shamt = imm12 & 0x3F;
+		rsp->regs[rd] = rsp->regs[rs1] >> shamt;
+	} else if (funct3 == 0b000) {
+		if (rs1 == 0) {
+			rsp->regs[rd] = imm12;		// li
+		} else if (imm12 == 0) {
+			rsp->regs[rd] = rsp->regs[rs1];   //mv
+		} else {
+			rsp->regs[rd] = rsp->regs[rs1] + imm12;
+		}
+	}
+
+	rsp->pc += 4;
+}
+
+void emu_u_type(struct rv_state *rsp, uint32_t iw) {
+	uint32_t rd = get_bits(iw, 7, 5);
+
+	int32_t imm20 = sign_extend(get_bits(iw, 12, 20), 19);
+
+	rsp->regs[rd] = imm20;
+
+	rsp->pc += 4;
+}
+
+void emu_j_type(struct rv_state *rsp, uint32_t iw) {
+	uint32_t rd = get_bits(iw, 7, 5);
+	int32_t imm20 = sign_extend(
+			get_bits(iw, 31, 1) << 11 |
+			get_bits(iw, 7, 1) << 6 |
+			get_bits(iw, 25, 6) << 4 |
+			get_bits(iw, 8, 4),
+			11
+	);
+
+	imm20 = (imm20 << 12) >> 12;
+
+	if (rd != 0) {
+		rsp->regs[rd] = rsp->pc + 4;
+	}
+
+	rsp->pc += imm20;
+}
+
+
+void emu_b_type(struct rv_state *rsp, uint32_t iw) {
+	uint32_t rs1 = get_bits(iw, 15, 5);
+	uint32_t rs2 = get_bits(iw, 20, 5);
+
+	uint32_t funct3 = get_bits(iw, 12, 3);
+	
+	int64_t imm12 = sign_extend(
+		get_bits(iw, 31, 1) << 12 |
+		get_bits(iw, 7, 1) << 11 |
+		get_bits(iw, 25, 6) << 5 |
+		get_bits(iw, 8, 4) << 1,
+		12
+	);
+	
+	if (funct3 == 0b001) { //bne 
+		if (rsp->regs[rs1] != rsp->regs[rs2]) {
+				rsp->pc += imm12;
+		} else {
+			rsp->pc += 4;
+		}
+	} else if (funct3 == 0b100) { //blt
+		if ((int64_t) rsp->regs[rs1] < (int64_t) rsp->regs[rs2]) {
+				rsp->pc += imm12;
+		} else {
+			rsp->pc += 4;
+		}
+	} 
+}
+
+
+void emu_jalr(struct rv_state *rsp, uint32_t iw) {
+	uint32_t rs1 = get_bits(iw, 15, 5);
+	uint64_t val = rsp->regs[rs1];
+
+	rsp->pc = val;
+}
+
 static void rv_one(rv_state *state) {
     uint32_t iw  = *((uint32_t*) state->pc);
     //iw = cache_lookup(&state->i_cache, (uint64_t) state->pc);
